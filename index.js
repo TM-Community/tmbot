d7d8356c192b538ab8b2519d9710199c2bf1ed72
Spirit <i8bou3@gmail.com>
2022-04-18 20:29:17 +0300

    Add event handler

--- index.js
+++ index.js
@@ -15,9 +15,16 @@ process.on("uncaughtExceptionMonitor", (e, o) => console.error(e, o));
 process.on("multipleResolves", (t, p, v) => console.error(t, v, p));
 
 const { Client } = require("discord.js");
+const { join } = require("path")
+const { loadFiles } = require("./fs");
 
 const client = new Client({
   intents: [],
+})
+
+loadFiles(join(__dirname, "events"), "js", { oneCollection: true }).each((event) => {
+  if (event.once) client.once(event.name, (...args) => event.execute(client, ...args));
+  else client.on(event.name, (...args) => event.execute(client, ...args));
 })
 
 client.login(process.env.TOKEN)
