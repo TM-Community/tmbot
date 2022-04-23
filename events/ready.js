26beff5367154d8f8ffa54cf5280c608f604e860
Spirit <i8bou3@gmail.com>
2022-04-23 05:59:26 +0300

    Load options for slash commands

--- events/ready.js
+++ events/ready.js
@@ -15,8 +15,8 @@ module.exports = {
     );
 
     setTimeout(async () => {
+      let count = 0;
       for (const guild of client.guilds.cache.values()) {
-        let count = 0;
         const locale = (await db.has("guilds", guild.id))
           ? (await db.get("guilds", guild.id)).locale
           : i18n.defaultLocale;
@@ -25,13 +25,65 @@ module.exports = {
           let commandData = {
             name: i18n.get(`${command.name}.name`, locale),
             description: i18n.get(`${command.name}.description`, locale),
-            options: command.options,
+            options: getOptions(command.name, command.options, locale) || null,
           };
           commands.push(commandData);
-          count++;
         }
+        count++;
         guild.commands.set(commands);
       }
+      log(i18n.get("client.slashCommands", i18n.defaultLocale, { count }));
     }, 10000);
   },
 };
+
+//--- DONT READ THIS SHIT IF YOU WANT TO KEEP YOUR EYES ---\\
+
+function getOptions(optionsPath, options, locale) {
+  let commandOptions = [];
+  if (options)
+    for (const option of options) {
+      let choices = [];
+      if (option.choices)
+        for (const choice of option.choices) {
+          let choiceData = {
+            name: i18n.get(
+              `${optionsPath}.${option.name}.${choice.value}`,
+              locale
+            ),
+            value: choice.value,
+          };
+          if (
+            [`${optionsPath}.${option.name}.${choice.value}`, ""].includes(
+              choiceData.name
+            )
+          )
+            choiceData.name = choice.name;
+          choices.push(choiceData);
+        }
+      let optionData = {
+        name: i18n.get(`${optionsPath}.${option.name}.name`, locale),
+        description: i18n.get(
+          `${optionsPath}.${option.name}.description`,
+          locale
+        ),
+        type: option.type,
+        required: option.required,
+        options: option.options
+          ? getOptions(`${optionsPath}.${option.name}`, option.options, locale)
+          : null,
+        choices: choices || null,
+      };
+      if ([`${optionsPath}.${option.name}.name`, ""].includes(optionData.name))
+        optionData.name = option.name;
+      if (
+        [`${optionsPath}.${option.name}.description`, ""].includes(
+          optionData.description
+        )
+      )
+        optionData.description =
+          option.description || "No description provided";
+      commandOptions.push(optionData);
+    }
+  return commandOptions;
+}
