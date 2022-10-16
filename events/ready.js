1c0f947308915a373c47a4a674a35df29be4117d
Spirit <i8bou3@gmail.com>
2022-10-16 18:56:11 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ const { Client } = require("discord.js");
 const i18n = require("../i18n");
 const { loadSlashCommands } = require("../loadSlashCommands");
 const { log } = require("../logger");
+const { db } = require("../db")
 
 module.exports = {
   name: "ready",
@@ -14,13 +15,16 @@ module.exports = {
       i18n.get("client.ready", i18n.defaultLocale, { user: client.user.tag })
     );
 
-    setTimeout(async () => {
-      let count = 0;
-      for (const guild of client.guilds.cache.values()) {
-        await loadSlashCommands(client, guild);
-        count++;
-      }
-      log(i18n.get("client.slashCommands", i18n.defaultLocale, { count }));
-    }, 10000);
+    if (db.ready) loadGuildsSlashCommands(client)
+    else db.once("ready", () => loadGuildsSlashCommands(client))
   },
 };
+
+async function loadGuildsSlashCommands(client) {
+  let count = 0;
+  for (const guild of client.guilds.cache.values()) {
+    await loadSlashCommands(client, guild);
+    count++;
+  }
+  log(i18n.get("client.slashCommands", i18n.defaultLocale, { count }));
+}
