9a74445c851755eb25a8dfd919b342b536b116a4
Spirit <i8bou3@gmail.com>
2022-10-18 22:02:15 +0300

    Add admin commands

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,65 @@
-const { Client, Message } = require("discord.js");
+const { Client, Message, ActivityType } = require("discord.js");
 const { db, Guild, Channel, User } = require("../db");
 const cooldown = require("../cooldown");
+
+const statusTypes = ["online", "dnd", "idle", "invisible"]
+const activityTypes = {
+  competing: ActivityType.Competing,
+  custom: ActivityType.Custom,
+  listening: ActivityType.Listening,
+  playing: ActivityType.Playing,
+  streaming: ActivityType.Streaming,
+  watching: ActivityType.Watching
+}
+
+/**
+* @param {Client} client
+* @param {Message} message
+*/
+function executeAdminCommand(client, message) {
+  const args = message.content.replace(client.prefix, "").split(/\s/)
+  const p = process.env.PREFIX ?? "="
+  const commandName = args.shift().toLowerCase()
+  const catchFunc = (r) => message.reply(`**Error:**\n> ${r}`)
+  switch (commandName) {
+    case "username":
+    case "name":
+      if (args[0]) {
+        client.user.setUsername(args.join(" "))
+          .then(() => message.reply("Username updated!"))
+          .catch(catchFunc)
+      } else message.reply(`**Usage:**\n> ${p}username <name>`)
+      break;
+    case "avatar":
+    case "icon":
+      const image = message.attachments?.first()
+      if (image || args[0]) {
+        client.user.setAvatar(image?.attachment ?? args[0])
+          .then(() => message.reply("Avatar updated!"))
+          .catch(catchFunc)
+      } else message.reply(`**Usage:**\n> ${p}avatar <link|attachment>`)
+      break;
+    case "status":
+      if (statusTypes.includes(args[0].toLowerCase())) {
+        client.user.setStatus(args[0].toLowerCase())
+          .then(() => message.reply("Status updated!"))
+          .catch(catchFunc)
+      } else message.reply(`**Usage:**\n> ${p}status (${statusTypes.join("|")})`)
+      break;
+    case "activity":
+      const type = args[0]?.toLowerCase()
+      const name = args.slice(1)?.join(" ")
+      if (name && Object.keys(activityTypes).includes(type)) {
+        client.user.setActivity({ name, type: activityTypes[type] })
+          .then(() => message.reply("Activity updated!"))
+          .catch(catchFunc)
+      } else message.reply(`**Usage:**\n> ${p}activity (${Object.keys(activityTypes).join("|")}) <name>`)
+      break;
+    default:
+      message.reply(`**Admin cmds:**\n> ${p}username, ${p}avatar, ${p}status, ${p}activity`)
+      break;
+  }
+}
 
 module.exports = {
   name: "messageCreate",
@@ -9,6 +68,7 @@ module.exports = {
    * @param {Message} message
    */
   async execute(client, message) {
+    if (client.admins?.includes(message.user.id) && message.content.test(client.prefix)) executeAdminCommand(client, message)
     const admin = message.member?.permissions.has("ManageGuild");
 
     /**
