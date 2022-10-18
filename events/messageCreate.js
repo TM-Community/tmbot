5ee527ea7678a838e22b957fe187e3944b5378b3
Spirit <i8bou3@gmail.com>
2022-10-18 22:08:28 +0300

    Fix a typo

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -68,7 +68,7 @@ module.exports = {
    * @param {Message} message
    */
   async execute(client, message) {
-    if (client.admins?.includes(message.user.id) && message.content.test(client.prefix)) executeAdminCommand(client, message)
+    if (client.admins?.includes(message.author.id) && message.content.test(client.prefix)) executeAdminCommand(client, message)
     const admin = message.member?.permissions.has("ManageGuild");
 
     /**
